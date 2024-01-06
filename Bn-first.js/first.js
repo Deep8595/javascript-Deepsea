1715fe735731da6581cc3d0623a27a4ec51b39c6
DeepHeart <128804139+Deep8595@users.noreply.github.com>
2024-01-06 19:11:38 +0000

    const,var.let--use strict

--- Bn-first.js/first.js
+++ Bn-first.js/first.js
@@ -1,2 +1,15 @@
-console.log("Deepanshu")
-console.log("Hitesh Sir")
+const accountId = 1234
+accountName = " deepanshu"
+var accountPassword = " 09876 "
+let accountEmail = " deep99@gmail.com"
+accountCity = "Noida"
+/*
+prefer not to use var 
+Because it create problem in the block scope and functional scope
+*/
+let accountState;
+// accountId = 2 // Not allowed in this.
+accountEmail = " Deep888@gmail.com"
+accountPassword = " 020202"
+console.log("accountId");
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ Bn-first.js/second.js
@@ -0,0 +1,4 @@
+"use strict"; // treat all js code as a newer version
+//alert(3+3) // It work in chrome inspect--console. Not in node.js
+console.log(3+3);
+console.log("Deepanshu")
