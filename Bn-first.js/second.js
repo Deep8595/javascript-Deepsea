db3fe66ecf48fd73b89da7a05163ad8689290e9d
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2024-01-08 10:22:34 +0000

    primitive DataType-- start

--- Bn-first.js/second.js
+++ Bn-first.js/second.js
@@ -2,3 +2,19 @@
 //alert(3+3) // It work in chrome inspect--console. Not in node.js
 console.log(3+3);
 console.log("Deepanshu")
+let name = " Hitesh "
+let age  = 21
+let isloggedIn = false 
+let state = null
+console.log(typeof " Hitesh")
+console.log(typeof null) // give a output(object)
+/* primitive datatype
+// number=> 2 to power 53
+// bigint
+// string=> "" -- This symbol is used in.
+// boolean => true/false
+// null=> standalone value
+// undefined=> 
+// symbol=> unique  */
+
+
