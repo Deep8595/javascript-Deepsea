54931a2415426cacf0ab18cf832f44fa1be49917
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2024-01-09 14:56:56 +0000

    conversion Operation

--- Bn-first.js/third.js
+++ Bn-first.js/third.js
@@ -1,6 +1,24 @@
-let score = "100"
-console.log( typeof score);
-console.log(typeof (score));
+let score = " 100 "
+//console.log( typeof score);
+//console.log(typeof (score));
+// valueInNumber = create a string in number form
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+// "100bc" give value not a number but type is a number
+// "100bc" => NaN
+// true=> 1; false=> 0;
+
+let isLoggedIn = ""
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log( booleanisLoggedIn );
+
+// boolean == ( 1=> true ; 0=> false)
+// "" =>  false
+// "Deepanshu" give a true value in boolean
+
+let someNumber = " 33 " 
+let Stringnumber = String(someNumber )
+console.log( Stringnumber )
+console.log(typeof  Stringnumber )
+                 
