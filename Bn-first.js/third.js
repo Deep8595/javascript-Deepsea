0f59e2849f27d3e2c2cc6656a870ab007b164f24
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2024-01-10 17:56:47 +0000

    Operator -- basic level completed

--- Bn-first.js/third.js
+++ Bn-first.js/third.js
@@ -35,5 +35,27 @@ let str3 = str1 + str2
 //console.log( "1" + 2 ); 
 //console.log( "1" + "1");    
 //console.log(2 +1 +" 3"); 
-console.log(  2 + 3  * 5 % 10 );
-        
+//console.log(  2 + 3  * 5 % 10 );
+//console.log( + true)
+//console.log ( + " "  )   
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2      
+let gameCounter = 10
+gameCounter ++ 
+//console.log ( gameCounter )
+let gameDecrement = 100 
+++ gameDecrement
+//console.log(gameDecrement)
+let numbX = 30
+numbX -- 
+//console.log(numbX);
+let numbY = 30
+numbY ++ 
+//console.log(numbY);
+let numbZ = 30 
+++ numbZ
+//console.log(numbZ);
+let numbA = 30
+-- numbA
+//console.log(numbA);
+
