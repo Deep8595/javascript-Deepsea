d5bad1b0b01a5477cea8f4f8c5fed8c202515f8f
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2024-01-10 18:38:48 +0000

    strict use operator( ===) in this.

---
+++ Bn-first.js/conditional.js
@@ -0,0 +1,16 @@
+//console.log( 2 > 1);
+//console.log ( 2 <= 1);
+//console.log( 2 != 1);
+//console.log( 2 < 1);
+//console.log( 2 == 1);
+//console.log( "a" > 2);
+//console.log( "2" < 5);
+//console.log( "02" > 3);
+console.log( null == 0);
+console.log( null != 0);
+console.log( null > 0);
+console.log( null < 0);
+console.log( null >= 0);
+console.log( null <= 0);
+// --- === ---  strict  use case , also check the datatype
+console.log(" ")
