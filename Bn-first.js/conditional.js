50d31353a5d6db7e73fb7810526f411d791ddc02
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2024-01-11 07:51:01 +0000

    conditional( null -- undefined )

--- Bn-first.js/conditional.js
+++ Bn-first.js/conditional.js
@@ -6,11 +6,23 @@
 //console.log( "a" > 2);
 //console.log( "2" < 5);
 //console.log( "02" > 3);
-console.log( null == 0);
-console.log( null != 0);
-console.log( null > 0);
-console.log( null < 0);
-console.log( null >= 0);
-console.log( null <= 0);
-// --- === ---  strict  use case , also check the datatype
-console.log(" ")
+ // below comparison may confuse in future better to avoid
+// console.log( null == 0);
+// console.log( null != 0);
+// console.log( null > 0);
+// console.log( null < 0);
+// console.log( null >= 0);
+// console.log( null <= 0);
+// --- === ---  strict  use case , also check the datatype and comparison
+// (" ") ---. string  
+// console.log( null === 0);
+// console.log( undefined == 0) // false
+// console.log( undefined >= 0) // false
+// console.log( undefined <= 0) // false
+// console.log( undefined < 0) // false
+// console.log( undefined > 0) // false
+// console.log( undefined != 0) // true
+// console.log( undefined === 0) // false
+
+
+
